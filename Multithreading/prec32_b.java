e061ff3af9ed415f50310d8a1faa1b1cd8e7143f
Tirth Jignesh Dalal <152633332+tiirth22@users.noreply.github.com>
2024-09-18 13:20:34 +0530

    Create prec32_b.java

---
+++ Multithreading/prec32_b.java
@@ -0,0 +1,14 @@
+class MyRunnable implements Runnable {
+    public void run() {
+        System.out.println("Hello World");
+    }
+}
+
+public class prec32_b {
+    public static void main(String[] args) {
+        MyRunnable m1 =  new MyRunnable();
+        Thread t1 = new Thread(m1);
+        t1.start();
+    }
+
+}
